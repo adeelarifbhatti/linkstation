292436f64308bbd8d462cb21fdcb24335a1bc0cd
Adeel Arif Bhatti <adeelarifbhatti@gmail.com>
2021-09-05 18:19:51 +0200

    Basic has been setup

---
+++ src/calculation/Calculation.java
@@ -0,0 +1,29 @@
+package calculation;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Points;
+import model.PowerStation;
+
+public class Calculation {
+	//List<PowerStation> powerStation = new ArrayList();
+	//List<Points> points = new ArrayList();
+
+
+	public static void calculatePower(List<PowerStation> pStation, List<Points> points ) {
+		
+		for (int i=0; i < pStation.size();i++) {
+			/*
+			System.out.println("Size is "+pStation.size());
+			System.out.println("X Y R is "+pStation.get(i).getX()+" "+pStation.get(i).getY()+" "+pStation.get(i).getR());
+			System.out.println("############################");
+			System.out.println("Size is "+points.size());
+			System.out.println("X Y is "+points.get(0).getX()+" "+points.get(0).getY());
+			*/
+			
+			
+		}
+	}
+
+
+}
---
+++ src/runnable/Runnable.java
@@ -0,0 +1,24 @@
+package runnable;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import calculation.Calculation;
+import model.Points;
+import model.PowerStation;
+
+public class Runnable {	
+	private static Scanner scan;	
+	public static void main(String args[]) {
+		scan = new Scanner(System.in);
+		List<PowerStation> powerStation = new ArrayList<PowerStation>();
+		List<Points> points = new ArrayList<Points>();
+		points.add(new Points(scan.nextInt(),scan.nextInt()));
+		powerStation.add(new PowerStation(0,0,10));
+		powerStation.add(new PowerStation(20,20,5));
+		powerStation.add(new PowerStation(10,0,12));		
+		Calculation.calculatePower(powerStation,points);
+	}
+
+}
